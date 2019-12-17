7ed2160367fab6e08be5a0f66ed5e0493c4dc0e7
Scott Silverman <scottms724@gmail.com>
2019-12-17 17:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
